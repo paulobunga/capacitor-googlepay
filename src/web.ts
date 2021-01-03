05260bb73141d1ab3cf112e23fa485b225450643
Paul Obunga <paulobunga.one@gmail.com>
2021-01-03 21:40:34 +0300

    Updated Code

--- src/web.ts
+++ src/web.ts
@@ -1,7 +1,9 @@
-import { WebPlugin } from '@capacitor/core';
+import { registerWebPlugin, WebPlugin } from '@capacitor/core';
 import { IonicGooglePayPlugin } from './definitions';
 
-export class IonicGooglePayWeb extends WebPlugin implements IonicGooglePayPlugin {
+export class IonicGooglePayWeb
+  extends WebPlugin
+  implements IonicGooglePayPlugin {
   constructor() {
     super({
       name: 'IonicGooglePay',
@@ -13,11 +15,16 @@ export class IonicGooglePayWeb extends WebPlugin implements IonicGooglePayPlugin
     console.log('ECHO', options);
     return options;
   }
+
+  async isReadyToPay() {}
+
+  async requestPayment(paymentDetails: any): Promise<any> {
+    return paymentDetails;
+  }
 }
 
 const IonicGooglePay = new IonicGooglePayWeb();
 
 export { IonicGooglePay };
 
-import { registerWebPlugin } from '@capacitor/core';
 registerWebPlugin(IonicGooglePay);
